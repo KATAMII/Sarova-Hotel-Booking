b15268e947c4f1297d9c11e5dbce8c37f3576c4c
KATAMII <lydiamwaah01@gmail.com>
2024-07-01 23:29:50 +0300

    add the testimonials part

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./Hero";
 import About from "./About";
 import Featured from "./Featured"
+import Testimonials from "./Testimonials"
 function Home() {
   return (
     <div>
       <Hero />
       <About />
       <Featured />
+      <Testimonials />
     </div>
   );
 }
