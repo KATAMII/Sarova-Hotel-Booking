eb1d73c1ad95b4a8508a61888d85eab697512c3b
KATAMII <lydiamwaah01@gmail.com>
2024-07-01 22:21:20 +0300

    finnish featured rooms

--- src/pages/Home/Featured.jsx
+++ src/pages/Home/Featured.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import roomimg from '../../assets/room1.jpeg';
+import studio from '../../assets/STUDIO.jpeg'
+import prince from '../../assets/prince.jpeg'
+import single from '../../assets/single.jpeg'
+import double from '../../assets/double.jpeg'
+import suite from '../../assets/suite.jpeg'
 import { MdLocationPin } from 'react-icons/md';
 import './Featured.css';
 import { TbHearts } from "react-icons/tb";
@@ -13,47 +18,62 @@ function Featured() {
       image: roomimg,
       title: 'Queens room',
       location: 'Queens plaza',
-      price: 6000,
+      price: 25000,
+      bath:3,
+      bed: 2,
     },
     {
       id: 2,
-      image: roomimg,
-      title: 'Queens room',
-      location: 'Queens plaza',
-      price: 6000,
+      image: studio,
+      title: 'Mia room',
+      location: 'Mia plaza',
+      price: 55000,
+      bath:5,
+      bed: 5,
     },
     {
       id: 3,
-      image: roomimg,
-      title: 'Queens room',
-      location: 'Queens plaza',
-      price: 6000,
+      image: prince,
+      title: 'Prince room',
+      location: 'Prince plaza',
+      price: 17000,
+      bath:1,
+      bed: 1,
     },
     {
       id: 4,
-      image: roomimg,
-      title: 'Queens room',
-      location: 'Queens plaza',
-      price: 6000,
+      image: double,
+      title: 'Double room',
+      location: 'Upper plaza',
+      price: 16000,
+      bath:2,
+      bed: 3,
     },
     {
       id: 5,
-      image: roomimg,
-      title: 'Queens room',
-      location: 'Queens plaza',
-      price: 6000,
+      image: single,
+      title: 'Single room',
+      location: 'Lower plaza',
+      price: 8000,
+      bath:1,
+      bed: 1,
     },
     {
       id: 6,
-      image: roomimg,
-      title: 'Queens room',
-      location: 'Queens plaza',
-      price: 6000,
+      image: suite,
+      title: 'Suite room',
+      location: 'Beach plaza',
+      price: 12000,
+      bath:2,
+      bed: 2,
     },
   ];
 
   return (
+    <div className="featured">
+        <h1 className="title">Featured Rooms</h1>
     <div className="room-list">
+       
       {rooms.map((room) => (
         <div key={room.id} className="room-card">
           <div className="romimg">
@@ -66,8 +86,8 @@ function Featured() {
               {room.location}
             </p>
             <div className="beds">
-              <p className='bed'>1 BED</p>
-              <p className='bed'> 1 BATH</p>
+              <p className='bed'>{room.bed}BED</p>
+              <p className='bed'> {room.bath} BATH</p>
               <p className='heart'><RiHeartsFill />
               </p>
             </div>
@@ -78,6 +98,7 @@ function Featured() {
           </div>
         </div>
       ))}
+    </div>
     </div>
   );
 }
